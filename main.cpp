b59ecab62d61e47d56c9b3adb4bfab6db0972604
David Elmo Ross <david.elmo.ross@icloud.com>
2024-08-02 20:32:02 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+    Title:  Program to generate output to the PC screen
+    Author: Your NAME, STUDENT ID, COURSE CODE, Lab Section
+    Date:   Today's Date
+    Description:    This program will ask for your NAME then output it centred
+                    on row 12. This program will also allow for Deleting chars
+                    (0x7F).
+*/
+#include "mbed.h"
+
+                         // Maximum Screen Size
+
+int main(void)
+{
+    printf("Hello World\n");
+   return(0);
+}
